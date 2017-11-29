c3e595f6eb91fd71434ee31df52549a5536cf8a4
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-11-29 11:11:53 -0500

    done reinf

--- nov_29_reinf.rb
+++ nov_29_reinf.rb
@@ -1,23 +1,23 @@
 require 'numbers_in_words'
 require 'numbers_in_words/duck_punch'
+require 'twitter_cldr'
 
-def ordinalizer(numb)
-  puts numb.in_words
-end
+
+puts "Please install twitter_cldr gemfile"
 
 def ordinalizer_deluxe(numb)
   puts "=-=-=-=-=-=-=-="
   expo = numb ** 2
   super_expo = numb ** 10
   puts numb
-  puts numb.in_words
-  puts expo.in_words
-  puts super_expo.in_words
+  puts numb.localize(:en).to_rbnf_s("SpelloutRules", "spellout-ordinal")
+  puts expo.localize(:en).to_rbnf_s("SpelloutRules", "spellout-ordinal")
+  puts super_expo.localize(:en).to_rbnf_s("SpelloutRules", "spellout-ordinal")
   puts "=-=-=-=-=-=-=-="
 end
 
 (1..20).each do |numbo|
-  ordinalizer(numbo)
+  ordinalizer_deluxe(numbo)
 end
 
 (25..30).each do |numbo|
